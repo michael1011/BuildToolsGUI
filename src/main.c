3f64da8faed2b71046bf7ac184f8d4632cbd5cd7
michael1011 <michael101101@me.com>
2017-01-22 15:20:24 +0100

    finished window

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 
 char name[] = "BuildToolsGUI";
 
+GtkTextBuffer *output;
+GtkTextIter iter;
+
 char* stradd(const char* a, const char* b){
     size_t len = strlen(a) + strlen(b);
 
@@ -13,28 +16,48 @@ char* stradd(const char* a, const char* b){
     return strcat(strcat(ret, a), b);
 }
 
+static void appendOutput(char toAppend[]) {
+    gtk_text_buffer_insert(output, &iter, stradd(toAppend, "\n"), -1);
+}
+
 static void winClosed() {
     exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char *argv[]) {
-    GtkBuilder *builder;
+static void createWin() {
     GtkWidget *window;
+    GtkBuilder *builder;
+
+    builder = gtk_builder_new_from_file("glade/main.glade");
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+
+    gtk_window_set_title(GTK_WINDOW(window), name);
+
+    gtk_builder_connect_signals(builder, NULL);
+    g_signal_connect(window, "delete_event", G_CALLBACK(winClosed), NULL);
+
+    output = gtk_text_view_get_buffer((GtkTextView *) gtk_builder_get_object(builder, "output"));
+
+    gtk_text_buffer_get_end_iter(output, &iter);
+
+    appendOutput("Checking if needed programs are installed...");
+
+    g_object_unref(builder);
+    gtk_widget_show(window);
+    gtk_main();
+}
+
+int main(int argc, char *argv[]) {
+    GtkApplication *app;
 
     gtk_init(&argc, &argv);
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "glade/main.glade", NULL);
+    app = gtk_application_new(stradd("at.michael1011", name), G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK(createWin), NULL);
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
-    gtk_builder_connect_signals(builder, NULL);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
 
-    g_signal_connect(window, "delete_event", G_CALLBACK(winClosed), NULL);
+    g_object_unref(app);
 
-    g_object_unref(builder);
-
-    gtk_widget_show(window);
-    gtk_main();
-
-    return 0;
+    return status;
 }
